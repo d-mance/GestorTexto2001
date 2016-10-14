99e617799c1e52b056f0901743f54b8cb67ef967
Daniel Mancera <dmance@edpac.org>
2016-10-14 17:35:36 +0200

    primer commit, el master

---
+++ src/gestortexto2001/model/FuncionesTexto.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestortexto2001.model;
+
+/**
+ *
+ * @author dmance
+ */
+public class FuncionesTexto {
+    
+    public static boolean search(String word)
+    {
+        
+        return false;
+    }
+    
+    public static String modifyText(String text, String word)
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        return sb.toString();
+    }
+    
+    public static String eraseWord(String text, String word)
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        return sb.toString();
+    }
+}
